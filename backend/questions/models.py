4d0bca3a3a848670d539dfe0960adfdac59c881e
op-guy <chughsaatvik@gmail.com>
2021-07-11 11:25:15 +0530

    Initial commit

---
+++ backend/questions/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+class Question(models.Model):
+    question = models.CharField(max_length=200)
+    topic = models.CharField(max_length=10)
+    answers = models.CharField(max_length=200)
+    correct_answer = models.CharField(max_length=100)
---
+++ backend/questions/views.py
@@ -0,0 +1,24 @@
+from django.shortcuts import render
+from django.http import HttpResponse, JsonResponse
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from rest_framework import status
+from .models import Question
+from .serializers import QuestionSerializer
+
+
+# Create your views here.
+@api_view(['GET', 'POST'])
+def all_questions_view(request):
+    if request.method == 'GET':
+        questions = Question.objects.all()
+        questions_serializer = QuestionSerializer(questions, many=True)
+        return Response(questions_serializer.data)
+
+    elif request.method == 'POST':
+        question_serializer = QuestionSerializer(data=request.data)
+        if question_serializer.is_valid():
+            question_serializer.save()
+            return Response(question_serializer.data, status=status.HTTP_201_CREATED)
+        return Response(question_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
